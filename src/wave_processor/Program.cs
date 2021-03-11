142129b42cfe907f133de74ac1beb236ae8ea378
Bas Suckling <Bas.Suckling@datacom.co.nz>
2021-03-12 08:56:51 +1300

    fix folder structure

---
+++ src/wave_processor/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace wave_processor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RawDataProcessor data = new RawDataProcessor();
+            data.getData();
+        }
+    }
+}
